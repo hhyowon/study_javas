104623dc2b50705a073ef07bce7707b4fe75767d
hhyowon <hyowon0605@gmail.com>
2023-05-23 16:13:10 +0900

    java오후

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main{
     public static void main(String[] args) {
         String name="Hyowon cho";
         System.out.println(name);
-        System.out.println("Hyowon cho");
+        Stirg name="cho";
+        System.out.println();
         //return 0;
     }
 }
---
+++ src/MakingGamebyOneHand.java
@@ -0,0 +1,24 @@
+public class MakingGamebyOneHand {
+    //- 변수 3개 선언(데이터 타입 상관 없음)
+    //변수 두 개에 각각 1과 2를 넣음    
+    //변수에 넣은 값은 변수로 교차 입력(직접 1,2 넣기 안됨)
+    public static void main(String[] args) {
+       int first=1;  
+       int second=0;
+       int third=2;  //변수 3개 선언
+
+       int Empty; //변수값 임시 저장할 빈공간
+
+       System.out.println("변경 전 : " +first+","+second+","+third);
+        Empty = second; //빈공간에 2번 변수값 임시 저장 
+        second = first; // 첫번째 숫자1 오른쪽 이동(second카드로 이동)
+        first = third; //세번째 카드 숫자(2) 첫번째 카드로 이동
+        third = second; //3번째 카드에 second카드로 이동시킨 숫자1 오른쪽 이동
+        second = Empty; //임시저장값 두번째카드에 입력
+
+       System.out.println("변경 후: " +first+","+second+","+third);
+        //return 0;
+    }
+
+    
+}
