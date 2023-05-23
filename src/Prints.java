cf6e6e6901a4aa221eb0794899e1bb2de145fbb7
hhyowon <hyowon0605@gmail.com>
2023-05-23 13:15:01 +0900

    javaprint

--- src/Prints.java
+++ src/Prints.java
@@ -1,6 +1,7 @@
 public class Prints {
     public static void main(String[] args) {
-        System.out.println("빅데이터 java 교육 과정");
+        String java="빅데이터 java 교육 과정";
+        System.out.println(java);
         //return 0;
     }
     
