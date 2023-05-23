75d928f2f3b194a86ca916539d719008885d3a1d
hhyowon <hyowon0605@gmail.com>
2023-05-23 13:09:26 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        String name="Hyowon cho";
+        System.out.println(name);
+        System.out.println("Hyowon cho");
+        //return 0;
+    }
+}
---
+++ src/Prints.java
@@ -0,0 +1,7 @@
+public class Prints {
+    public static void main(String[] args) {
+        System.out.println("빅데이터 java 교육 과정");
+        //return 0;
+    }
+    
+}
