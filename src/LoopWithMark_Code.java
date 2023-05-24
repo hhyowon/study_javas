e8c22d078d288204477def0c8833fce477b5dcfb
hhyowon <hyowon0605@gmail.com>
2023-05-24 15:24:07 +0900

    for end

---
+++ src/LoopWithMark_Code.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class LoopWithMark_Code{
+    public static void main(String[] args){
+        for(int line=1; line <6; line=line+1)
+        {
+                System.out.println("for "+line);
+            }
+
+            System.out.println("end");
+        }
+    }
+
+   /*  public static void main(String[] args){
+        for(int line=1; line <6; line=line+1)
+        {
+            for(int linecount=1; linecount<line; linecount=linecount+1)
+            {
+                System.out.print("?");
+            }
+              System.out.println();
+        }      
+    }*/
