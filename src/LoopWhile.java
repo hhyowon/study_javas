223bf778ed5b04825c235193a925e4c5767b17ac
hhyowon <hyowon0605@gmail.com>
2023-05-24 10:55:42 +0900

    loop

---
+++ src/LoopWhile.java
@@ -0,0 +1,14 @@
+public class LoopWhile {
+    public static void main(String[] args) {
+        //while (condition) {
+            // code block to be executed
+          //}
+          int first = 5;
+          int second = 10;
+            while (first < second) {
+                System.out.println(first);
+                 first= first + 2; 
+          }
+          System.out.println();
+    }
+}
---
+++ src/LoopWithCounter.java
@@ -0,0 +1,11 @@
+public class LoopWithCounter {
+    public static void main(String[] args) { //5까지 1씩 증가하는 값 찍기
+        int first=1;
+        while(first<6)
+        {
+            System.out.println(first);
+                 first= first + 1; 
+        }
+    }
+
+}
