5850f69a590045b347090cd43db007ca8933b1ed
hhyowon <hyowon0605@gmail.com>
2023-05-24 13:08:25 +0900

    plus

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        String name="Hyowon cho";
-        System.out.println(name);
-        Stirg name="cho";
-        System.out.println();
+        //refer : https://www.acmicpc.net/submit/2557
+        System.out.println("Hello World를 출력하시오."); //문제 : Hello World를 출력하시오.
         //return 0;
     }
 }
