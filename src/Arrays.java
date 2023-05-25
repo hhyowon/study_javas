bfd8afe8ab945244f15afdf3e5535249b6d5b96d
hhyowon <hyowon0605@gmail.com>
2023-05-25 09:52:12 +0900

    good

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,10 +1,9 @@
 public class Arrays {
     public static void main(String[] args) {
-        String [] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for(int num=0;num<5;num=num+1){
-               System.out.println(cars[num]);
+        String [] cars = {"Volvo", "BMW", "Ford", "Mazda"}; //arrays
+        for(int num=0;num<4;num=num+1){   //for문으로 배열 나열하기
+               System.out.println(cars[num]);  //배열은 0부터 시작이니깐 num=0 
         }
-     
         //return 0;
     }
 }
