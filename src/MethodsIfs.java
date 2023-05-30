65f27d4ab2c0e50f2191773fcb2afad03032c506
hhyowon <hyowon0605@gmail.com>
2023-05-30 18:27:53 +0900

    methodifs

---
+++ src/MethodsIfs.java
@@ -0,0 +1,49 @@
+import java.util.Scanner; 
+
+public class MethodsIfs {
+    //와일 무한반복하고 , 0되면 끝나기
+    public String iftime(int time){
+        String str ="";
+        try {
+            if(time < 12){
+                str = "오전입니다";
+            }
+            else if(time < 18){
+                str = "오후입니다";
+            }
+            else{
+                str = "밤입니다";
+            }      
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return str; 
+
+    }
+        
+    public static void main(String[] args) {
+        
+        try {
+            boolean loops = true;
+
+            while(loops){ //무한루프 
+                Scanner myObj = new Scanner(System.in); 
+                System.out.print("몇시입니까? : ");
+                int time = myObj.nextInt(); //시간 입력 스캐너
+                if (time == 0) {
+                    System.out.println("종료합니다.");
+                   break; // 무한루프 나가기
+                } else {
+                MethodsIfs  methodsIfs = new MethodsIfs(); // (클래쓰)데이터타입 변수이름 = new 클래스이름 _ function을 호출하기 위해 instants화함
+                String returnresult = methodsIfs.iftime(time); //값을 넣어줄 새로운 string 변수 선언 후 값 넣음
+                System.out.println(returnresult);
+                }
+            }
+          
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}
--- src/beak/Main.java
+++ src/beak/Main.java
@@ -4,44 +4,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in); 
-    String  [] bucket ={"","","",""," "} ;
-    int [] bucket_price  ={};
+        // 18108 :  https://www.acmicpc.net/problem/18108
+        // 불기연도에서 서기연도 출력하는 프로그램 
+        // 서기연도 = 불기연도-543년
+        // Scanner myObj = new Scanner(System.in); 
+        // int year =  myObj.nextInt();
+        // System.out.println(year-543); 
 
-    int i;
-    System.out.println("장바구니(최대 5개 까지 가능)");
-
-            for(i=0; i<5 ; i=i+1){
-                System.out.print((i+1)+" 번째 담기 :");
-                bucket[i] = myObj.nextLine();     
-            }
-
-            System.out.println("--- 담은 내용물 --- ");
-
-            for(i=0;i<5;i=i+1){ //담은 내용물을 보기 위한 for문
-                System.out.print(bucket[i]+",");
-            }
-
-            System.out.println("--- 계산 시작 --- ");
-            for(i=0;i<bucket.length;i=i+1){ //가격입력을 위한 for문
-                System.out.print((i+1)+" 번째 가격 :");
-                bucket_price[i] = myObj.nextInt();  
-
-                if(bucket_price[i] > 100){
-                    System.out.println("100원 초과 0원 적용");
-                    bucket_price[i] = 0;
-                }else if(bucket_price[i] == 0){
-                    break;                     
-                }  
-                
-                
-            }
-
-            System.out.println("--- 담은 총합 --- ");
-            System.out.println(" 합계 : ");
-            System.out.println("--- 계산 종료 --- ");
-
-
-    }
+        // 10430 : https://www.acmicpc.net/problem/10430 
+        // 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램
+        // Scanner myObj = new Scanner(System.in); 
+        // int A = myObj.nextInt();
+        // int B = myObj.nextInt();
+        // int C = myObj.nextInt();
     
-}
+        // System.out.println((A+B)%C);
+        // System.out.println(((A%C) + (B%C))%C);
+        // System.out.println( (A*B)%C);
+        // System.out.println (((A%C)*(B%C))%C);
+
+        // 2588 : https://www.acmicpc.net/problem/2588
+        // (세 자리 수) × (세 자리 수) 과정 보이는 프로그램 작성하기
+
+        Scanner myObj = new Scanner(System.in); 
+         int A = myObj.nextInt();
+         int B = myObj.nextInt();
+
+         System.out.println(A*(B%38));
+        
+
+
+
+
+        }
+    }
+
