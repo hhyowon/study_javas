75f0f8d40dc0151386210f7a0a78ae594fd3dfde
hhyowon <hyowon0605@gmail.com>
2023-05-30 09:32:14 +0900

    good

--- src/cases/CasherFor.java
+++ src/cases/CasherFor.java
@@ -6,13 +6,13 @@ public class CasherFor {
         try{  
             Scanner myObj = new Scanner(System.in); 
             int [] bucket_price = new int[5]; //5개까지만 담기
-            int i; //for문 돌리기 위한 변수
             int sum =0; //합을 넣기 위한 sum 변수
+
 
             System.out.println("장바구니(최대 5개 까지 가능)"); 
     
                     System.out.println("\n--- 계산 시작 --- ");
-                    for(i=0;i<bucket_price.length;i=i+1){ //가격입력을 위한 for문
+                    for( int i=0;i<bucket_price.length;i=i+1){ //가격입력을 위한 for문
                         System.out.print((i+1)+" 번째 가격 :");
                         bucket_price[i] = myObj.nextInt();  //배열에 가격입력값 넣기 
     
@@ -21,13 +21,10 @@ public class CasherFor {
                             bucket_price[i] = 0;  //0원 적용
                         }else if(bucket_price[i] == 0){ //0일때 나감
                             break;             
-                        }                                     
+                        }    
+                         sum =  sum + bucket_price[i];  //가격합구하기
                     }
-                    
-                    for(i=0;i<bucket_price.length;i=i+1){               //가격합산을 위한 for문
-                        sum =  sum + bucket_price[i]; 
-                    }
-                    
+                                      
                     System.out.println("--- 담은 총합 --- ");
                     System.out.println(" 합계 : "+sum ); //합계출력
                     System.out.println("\n--- 계산 종료 --- ");
