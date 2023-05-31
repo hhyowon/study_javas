d7401688ca4b6bf2e094bfe6f49657216c4c628a
Your Name <you@example.com>
2023-05-31 12:30:20 +0900

    효원님 깃허브

---
+++ src/Kmj.java
@@ -0,0 +1,13 @@
+public class Kmj {
+    
+    public static void main(String[] args) {
+        try {
+            
+            System.out.println("김민정 방문합니다 ");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        //result 0; 
+    }
+}
