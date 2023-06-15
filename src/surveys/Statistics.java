835b99932ab9249613afa49efe0187d4f8ca016e
hhyowon <hyowon0605@gmail.com>
2023-06-15 17:19:27 +0900

    아임효원

---
+++ src/surveys/Statistics.java
@@ -0,0 +1,26 @@
+package surveys;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Statistics {
+    public int getRespondents(Statement statement){
+        try {
+            System.out.println("---통계---");
+            // -- 총 설문자 : 3명
+            String queryB = "SELECT COUNT(*) CNT\n" + //
+                    "FROM (\n" + //
+                    "\tSELECT RESPONDENTS_ID, COUNT(*) CNT\n" + //
+                    "\tFROM statistics\n" + //
+                    "\tgroup by RESPONDENTS_ID\n" + //
+                    "     ) AS T_STATIC\n";
+            ResultSet resultSet = statement.executeQuery(queryB);
+            while (resultSet.next()) {
+                System.out.println("총 설문자 : " + resultSet.getString("CNT")+"명");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+}
