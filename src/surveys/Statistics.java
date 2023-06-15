0ba9bf4660f482f60d1c1d28bc0fc98527236021
hhyowon <hyowon0605@gmail.com>
2023-06-15 17:19:27 +0900

    ggg

--- src/surveys/Statistics.java
+++ src/surveys/Statistics.java
@@ -1,27 +1,12 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+package surveys;
+
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-/**
-surveyapp
-*/
-public class SurveyApp {
-    
-    public static void main(String[] args) {
-        
-        try {// - MySQL workbench 실행 : JDBC
-            // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://127.0.0.1:3306/db_survey";
-            String user = "root";
-            String password = "!yojulab*";
 
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("DB연결 성공\n");
-
-            // - query Edit
-            Statement statement = connection.createStatement();
-
+public class Statistics {
+    public int getRespondents(Statement statement){
+        try {
             System.out.println("---통계---");
             // -- 총 설문자 : 3명
             String queryB = "SELECT COUNT(*) CNT\n" + //
@@ -34,10 +19,9 @@ public class SurveyApp {
             while (resultSet.next()) {
                 System.out.println("총 설문자 : " + resultSet.getString("CNT")+"명");
             }
-            
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            // TODO: handle exception
         }
+        return 0;
     }
 }
